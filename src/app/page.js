22b5573897e900c645d2ee9b822175311e2c1b48
OpaogunProsper <prblvck@gmail.com>
2024-08-19 14:01:19 +0100

    spiking API endpoints

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
-import Image from "next/image";
+'use client'
 import Link from "next/link";
+import { useEffect } from "react";
+import { getRecipes } from "../../api";
 
 export default function Home() {
+  useEffect (() => {
+    const res = getRecipes()
+    console.log(res)
+  }, [])
   return (
    <main>
     <h1>Hello</h1>
     <Link href='/photos' >Photos</Link>
-   
+    
    </main>
   );
 }
